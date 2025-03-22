5096fab617a279c2c269d70a1d9ceb569b80e63b
Michael Lindre <michael.lindre@oodesigns.co.uk>
2025-03-22 13:55:53 +0000

    fix: updated text to path due to linux differences

--- src/components/SelectSwitch/VentMode.jsx
+++ src/components/SelectSwitch/VentMode.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const VentMode = ({path, textPath,  text, dominantBaseline, isSelected, onSelect,
-                      }) => {
+const VentMode = ({id, path, textPath,  isSelected, onSelect}) => {
 
     const handleClick = () => {
-        onSelect(text.toLowerCase());
+        onSelect(id.toLowerCase());
     };
-
-    // Create a unique textPathId based on the text prop
-    const textPathId = `vent-mode-text-id-${text.toLowerCase()}`;
 
     return (
         <g className="vent-mode" role="button"  onClick={handleClick}>
@@ -17,17 +13,9 @@ const VentMode = ({path, textPath,  text, dominantBaseline, isSelected, onSelect
                 className={`vent-mode-path ${isSelected ? 'selected' : ''}`}
                 d={path}
             />
-            <path id={textPathId} style={{ fill: 'none'}} d={textPath} />
-            <text className="vent-mode-text">
-                <textPath
-                    xlinkHref={`#${textPathId}`}
-                    startOffset="50%"
-                    textAnchor="middle"
-                    dominantBaseline={dominantBaseline || undefined}
-                >
-                    {text}
-                </textPath>
-            </text>
+            <path className="vent-mode-text"
+                  d={textPath}
+            />
         </g>
     );
 };
@@ -35,8 +23,7 @@ const VentMode = ({path, textPath,  text, dominantBaseline, isSelected, onSelect
 VentMode.propTypes = {
     path: PropTypes.string.isRequired,
     textPath: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
-    dominantBaseline: PropTypes.string,
+    id: PropTypes.string.isRequired,
     isSelected: PropTypes.bool.isRequired,
     onSelect: PropTypes.func.isRequired,
 }
